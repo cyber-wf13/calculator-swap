b717860a82f5457dad7bfef75def42dfaf4e5fa2
cyber-wf13 <o.zon77788@gmail.com>
2024-11-30 02:25:58 +0200

    Add API requests to retrieve codes and currency rates

--- src/js/logical/Exchange.js
+++ src/js/logical/Exchange.js
@@ -1,1 +1,41 @@
-export class Exchange {}
+export class Exchange {
+  constructor() {
+    this.fillCurrencyInfo().then((info) => {
+      this.currencyInfo = info;
+      // console.log(this.currencyInfo);
+    });
+  }
+
+  async request(url) {
+    const res = await fetch(url);
+    if (res.ok) {
+      return await res.json();
+    }
+  }
+
+  async fillCurrencyInfo() {
+    return this.request(
+      "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json",
+    ).then((res) => {
+      return res.map((currency) => {
+        return {
+          "name": currency["cc"],
+          "code": currency["r030"],
+          "txt": currency["txt"],
+        };
+      });
+    });
+  }
+
+  async fillCurrencyExchange(code) {
+    return this.request("http://localhost:3000/monobank.json").then(
+      (currency) => {
+        for (const idx in currency) {
+          if (currency[idx]["currencyCodeA"] == code) {
+            return currency[idx];
+          }
+        }
+      },
+    );
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import { Calc } from "./form/Calc";
 import { Select } from "./form/Select";
+import { Exchange } from "./logical/Exchange";
 import { closeByAnyClick } from "./utils";
 
 const calc = new Calc(
@@ -23,3 +24,8 @@ const payInput = document.querySelector("#pay");
 const receiveInput = document.querySelector("#receive");
 payInput.after(paySelect.elem);
 receiveInput.after(receiveSelect.elem);
+
+const ex = new Exchange();
+ex.fillCurrencyExchange(51).then((curr) => {
+  console.log(curr);
+});
